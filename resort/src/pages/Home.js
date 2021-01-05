054f1ee87810734c1faa6b0a3b0c21bdb53d2603
Ankita <Ankita.bhatnagar@outlook.com>
2021-01-05 20:17:43 +0200

    updates

--- resort/src/pages/Home.js
+++ resort/src/pages/Home.js
@@ -1,9 +1,17 @@
 import React from 'react'
-
-export default function home() {
-    return <div>Hello from home page</div>
+import Hero from "../components/Hero";
+import Banner from "../components/Banner"
+import {Link} from "react-router-dom";
+export default function Home() {
+    return <Hero>
+        <Banner title ="Luxurious Rooms" subtitle ="Deluxe Rooms Starting At €85">
+            <Link to ="/rooms" className = "btn-primary">
+                Our rooms
+            </Link>
+        </Banner>
+        </Hero>;
         
-            
+   
         
     
 }
