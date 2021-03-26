882063a0d0134e7bf40770757866d80eef7da0cb
Ankita Bhatnagar <ankita.bhatnagar@outlook.com>
2021-03-26 17:16:16 +0200

    adding css to boxes

--- react-task-1/src/App.js
+++ react-task-1/src/App.js
@@ -4,9 +4,31 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-    <h1>Hello World!</h1>
-    </div>
+ <div>
+   <header>
+     <h1>LOGO</h1>
+   </header>
+   <main>
+     <div className ="box">
+       <h2>Name: some name</h2>
+       <p>Age:some age</p>
+       <p>Title</p>
+     </div>
+     <div className ="box">
+       <h2>Name: some name</h2>
+       <p>Age:some age</p>
+       <p>Title</p>
+     </div>
+     <div className ="box">
+       <h2>Name: some name</h2>
+       <p>Age:some age</p>
+       <p>Title</p>
+     </div>
+   </main>
+   <footer>
+     <p>Copyright</p>
+   </footer>
+ </div>
   );
 }
 
